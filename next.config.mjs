806fce7d0a1d5b1fffe61391976c91a1604586f7
Rae-PoolUp <raeezy1912@gmail.com>
2026-09-11 20:10:41 +0100

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const config = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: (process.env.NEXT_PUBLIC_BASE_PATH || '').replace(/\/$/, ''),
-  images: { unoptimized: true },
-  poweredByHeader: false
-};
-export default config;
